f08afc456a5fe01bcd35eeeb03ce3d926a086a56
ariffmunshi <9564774-ariffmunshi@users.noreply.replit.com>
2023-09-02 05:39:27 +0000

    Ex4.2: Create reverse functionality

--- src/BigCats.jsx
+++ src/BigCats.jsx
@@ -3,13 +3,13 @@ import SingleCat from "./SingleCat"
 
 function BigCats() {
   const cats = [
-  { name: 'Tiger', latinName: 'Panthera tigris', imageURL: "http://placekitten.com/120/120" },
   { name: 'Cheetah', latinName: 'Acinonyx jubatus', imageURL: "http://placekitten.com/200/120" },
   { name: 'Cougar', latinName: 'Puma concolor', imageURL: "http://placekitten.com/160/120" },
   { name: 'Jaguar', latinName: 'Panthera onca', imageURL: "http://placekitten.com/180/120" },
   { name: 'Leopard', latinName: 'Panthera pardus', imageURL: "http://placekitten.com/220/120" },
   { name: 'Lion', latinName: 'Panthera leo', imageURL: "http://placekitten.com/160/120" },
   { name: 'Snow leopard', latinName: 'Panthera uncia', imageURL: "http://placekitten.com/140/120" },
+  { name: 'Tiger', latinName: 'Panthera tigris', imageURL: "http://placekitten.com/120/120" },
 ]
 
   const [catList, setCatList] = useState([...cats])
@@ -19,10 +19,17 @@ function BigCats() {
     newList.sort((a, b) => a.name < b.name ? -1 : 1)
     setCatList(newList)
   }
+
+  function handleReverse() {
+    const newList = [...catList]
+    newList.reverse()
+    setCatList(newList)
+  }
  
   return (
    <>
    <button onClick={handleSort}>Sort</button>
+   <button onClick={handleReverse}>Reverse</button>
    <ul style={{display: "flex"}}>
      {catList.map((cat, i) => (<SingleCat key={i} cat={cat}/>))}
    </ul>
